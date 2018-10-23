40908f2e4e2b67f626a24ebb0528d9f4f9476c43
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-10-23 14:31:16 -0500

    Working on aggregating the historical information

---
+++ src/server/routes/courses/getProfessors.ts
@@ -0,0 +1,49 @@
+import {Request, Response} from 'express';
+import {sectionModel} from '../../models/courses/Section';
+import { Aggregate } from 'mongoose';
+
+type InstructorAggregation = {
+    _id: {
+        instructor: string,
+        index: number
+    },
+    grades: number
+}
+
+type instructorSummary = {
+    instructor: string,
+    ABCDFISUQX: number[],
+    GPA: number
+}
+
+export async function getProfessors(req: Request, res: Response){
+    const {dept, courseNum}: {dept: string, courseNum: string} = req.params;
+    let instructorSummary: instructorSummary = {}
+    let aggregationOperation: Object[] = [
+        {$match:
+            {$and: [
+                {dept: dept},
+                {courseNum: courseNum}
+            ]}
+        },
+        {$unwind: {
+            path: "$gradeDistribution.grades",
+            includeArrayIndex: "index"
+        }},
+        {$group: {
+            _id: {instructor: "$instructor", index: "$index"},
+            grades: {$sum : "$gradeDistribution.grades"}
+        }}
+    ];
+    const results: InstructorAggregation[] = await sectionModel.aggregate(aggregationOperation);
+    for (let result of results){
+        const instructor: string = result["_id"].instructor;
+        if (instructor in instructorSummary){
+            
+        }
+        else{
+            instructorSummary[instructor] = 
+        }
+    }
+    console.log(results);
+}
