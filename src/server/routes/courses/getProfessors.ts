8e8d06d2a49dad3cae2abe925b0605caf8382ea1
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-10-23 23:51:07 -0500

    Completed aggregating the data for the professor route

--- src/server/routes/courses/getProfessors.ts
+++ src/server/routes/courses/getProfessors.ts
@@ -2,10 +2,19 @@ import {Request, Response} from 'express';
 import {sectionModel} from '../../models/courses/Section';
 import { Aggregate } from 'mongoose';
 
-type InstructorAggregation = {
+type SummaryAggregation = {
     _id: {
         instructor: string,
+        termCode: number,
         index: number
+    },
+    grades: number
+}
+
+type HistoryAggregation = {
+    _id: {
+        instructor: string,
+        termCode: number
     },
     grades: number
 }
@@ -13,48 +22,77 @@ type InstructorAggregation = {
 interface InstructorSummary{
     [key: string]: {
         ABCDFISUQX: number[],
-        GPA: number
+        GPA: number,
+        history: {
+            [key: string]: {
+                ABCDFISUQX: number[],
+                GPA: number
+            }
+        }
     }
 }
 
 export async function getProfessors(req: Request, res: Response){
     const {dept, courseNum}: {dept: string, courseNum: string} = req.params;
-    let instructorSummary: InstructorSummary = {}
-    let aggregationOperation: Object[] = [
-        {$match:
-            {$and: [
-                {dept: dept},
-                {courseNum: courseNum}
-            ]}
-        },
-        {$unwind: {
-            path: "$gradeDistribution.grades",
-            includeArrayIndex: "index"
-        }},
-        {$group: {
-            _id: {instructor: "$instructor", index: "$index"},
-            grades: {$sum : "$gradeDistribution.grades"}
-        }}
-    ];
-    const results: InstructorAggregation[] = await sectionModel.aggregate(aggregationOperation);
-    console.log(results);
-    for (let result of results){
-        const instructor: string = result["_id"].instructor;
-        if (!(instructor in instructorSummary)){
-            instructorSummary[instructor] = {
-                ABCDFISUQX: [0,0,0,0,0,0,0,0,0,0],
-                GPA: 0
-            }
-        }
-        instructorSummary[instructor].ABCDFISUQX[result._id.index] = result.grades;
-    }
-    for (let key of Object.keys(instructorSummary)){
-        instructorSummary[key].GPA = getGPA(instructorSummary[key].ABCDFISUQX);
-    }
-    res.json(instructorSummary);
+    const result = await getSummaryInfo(dept, courseNum);
+    res.json(result);
 }
 
-function getGPA(grades: number[]){
+async function getSummaryInfo(dept: string, courseNum: string){
+    try{
+        let instructorSummary: InstructorSummary = {}
+        let summaryAggregationOperation: Object[] = [
+            {$match:
+                {$and: [
+                    {dept: dept},
+                    {courseNum: courseNum}
+                ]}
+            },
+            {$unwind: {
+                path: "$gradeDistribution.grades",
+                includeArrayIndex: "index"
+            }},
+            {$group: {
+                _id: {instructor: "$instructor", index: "$index", termCode: "$termCode"},
+                grades: {$sum : "$gradeDistribution.grades"}
+            }}
+        ];
+        const summaryResults: SummaryAggregation[] = await sectionModel.aggregate(summaryAggregationOperation);
+        for (let result of summaryResults){
+            const instructor: string = result["_id"].instructor;
+            if (!(instructor in instructorSummary)){
+                instructorSummary[instructor] = {
+                    ABCDFISUQX: [0,0,0,0,0,0,0,0,0,0],
+                    GPA: 0,
+                    history: {}
+                }
+            }
+            instructorSummary[instructor].ABCDFISUQX[result._id.index] += result.grades;
+
+            const termCode: number = result["_id"].termCode;
+            if (!(termCode in instructorSummary[instructor].history)){
+                instructorSummary[instructor].history[termCode] = {
+                    ABCDFISUQX: [0,0,0,0,0,0,0,0,0,0],
+                    GPA: 0
+                }
+            }
+            instructorSummary[instructor].history[termCode].ABCDFISUQX[result._id.index] = result.grades;
+        }
+        for (let summaryKey of Object.keys(instructorSummary)){
+            instructorSummary[summaryKey].GPA = getGPA(instructorSummary[summaryKey].ABCDFISUQX);
+            for (let historyKey of Object.keys(instructorSummary[summaryKey].history)){
+                instructorSummary[summaryKey].history[historyKey].GPA = getGPA(instructorSummary[summaryKey].history[historyKey].ABCDFISUQX);
+            }
+        }
+        return instructorSummary;
+    }
+    catch(err){
+        console.log(err);
+        return {};
+    }
+}
+
+function getGPA(grades: number[]): number{
     const sum: number = grades.slice(0, 5).reduce((a: number, b: number) => a + b, 0);
     return (grades[0] * 4.0 + grades[1] * 3.0 + grades[2] * 2.0 + grades[3] * 1.0)/(sum);
 }
